044d652568853664a6cba3337f72bd61b2886af2
Unai Elizondo <unai.elizondo@biko2.com>
2021-11-26 17:55:32 +0100

    Garrafa

--- src/Components/NotPageFound.tsx
+++ src/Components/NotPageFound.tsx
@@ -1,11 +1,11 @@
 function NotPageFound() {
   return (
     <div className="container mt-5">
-      <div className="">
-        <div className="col align-self-center">
-          <h1 className="">Not Page Found Ouu!</h1>
+   <div className="">
+   <div className="col align-self-center">
+     <h1 className="">Not Page Found Ouu!</h1>
         </div>
-      </div>
+ </div>
     </div>
   );
 }
