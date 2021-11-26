155e6ee0cfdf9b2314d652b6989b227218dda8d7
Unai Elizondo <unai.elizondo@biko2.com>
2021-11-26 20:25:17 +0100

    PANPARRI

--- src/Components/NotPageFound.tsx
+++ src/Components/NotPageFound.tsx
@@ -1,6 +1,7 @@
 function NotPageFound() {
   return (
     <div className="container mt-5">
+      {" "}
       <div className="">
         <div className="col align-self-center">
           <h1 className="">Not Page Found Ouu!</h1>
