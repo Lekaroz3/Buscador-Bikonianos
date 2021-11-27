8a7ed8adaad41cce34c3340845958990a6bae5c3
Unai Elizondo <unai.elizondo@biko2.com>
2021-11-27 09:42:47 +0100

    SD

--- src/Components/NotPageFound.tsx
+++ src/Components/NotPageFound.tsx
@@ -2,11 +2,11 @@ function NotPageFound() {
   return (
     <div className="container mt-5">
       {" "}
-   <div className="">
-     <div className="col align-self-center">
-  <h1 className="">Not Page Found Ouu!</h1>
-    </div>
-   </div>
+      <div className="">
+        <div className="col align-self-center">
+          <h1 className="">Not Page Found Ouu!</h1>
+        </div>
+      </div>
     </div>
   );
 }
